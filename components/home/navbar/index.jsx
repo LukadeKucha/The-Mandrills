c7d4502eae45d69e8cf138c5612ea98b33bafa44
gonjila <lgonjila@gmail.com>
2022-02-28 01:24:41 +0400

    made unresponsive navbar

--- components/home/navbar/index.jsx
+++ components/home/navbar/index.jsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
+
 import styles from "./styles.module.scss";
 
 function HomeNavbar() {
-  return <div>HomeNavbar</div>;
+  return (
+    <div className={styles.navbarWrapper}>
+      <div className={styles.logoWrapper}>
+        <Image src="/images/logo.png" alt="logo" width={142} height={142} />
+
+        <div className={styles.companyName}>
+          <div>THE</div>
+          <div>MANDRILLS</div>
+        </div>
+      </div>
+
+      <div className={styles.sectionsList}>
+        <div className={styles.column}>
+          <div>NFT’S</div>
+          <div>ROADMAP</div>
+          <div>WN</div>
+        </div>
+        <div className={styles.column}>
+          <div>“The Mandrill Game”</div>
+          <div>TMMS</div>
+        </div>
+        <div className={styles.column}>
+          <div>FAQ</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomeNavbar;
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,7 +1,25 @@
 import styles from "./styles.module.scss";
 
+import HomeFooter from "../../components/home/footer";
+import HomeNavbar from "../../components/home/navbar";
+import ArtistSection from "../../components/home/artist";
+import RoadMapSection from "../../components/home/road-map";
+import WildNetworkSection from "../../components/home/wild-network";
+import FaqSection from "../../components/home/FAQ";
+
 function HomePage() {
-  return <div className={styles.HomePageWrapper}>HomePage</div>;
+  return (
+    <div className={styles.HomePageWrapper}>
+      <HomeNavbar />
+      <div className={styles.backgroundSection}>
+        <ArtistSection />
+        <RoadMapSection />
+        <WildNetworkSection />
+        <FaqSection />
+      </div>
+      <HomeFooter />
+    </div>
+  );
 }
 
 export default HomePage;
